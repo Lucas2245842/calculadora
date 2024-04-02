96fe3a94fe3b23a607fc52659af5396ee4bc440f
Lucas2245842 <162581164+Lucas2245842@users.noreply.github.com>
2024-04-02 16:06:25 -0300

    Add arquivos calculadora

---
+++ aulaPratica2.ts
@@ -0,0 +1,67 @@
+let inOut: number = 0;
+
+do{
+    const teclado = require(`prompt-sync`)();
+
+    console.log(`Qual operação deseja?`);
+    console.log(`+ --> Adição`);
+    console.log(`- --> Subtração`);
+    console.log(`* --> Multiplicação`);
+    console.log(`/ --> Divisão`);
+
+    let opcao: string = teclado(`Digite a opção desejada: `);
+
+    let numeroUm: number = 0;
+    let numeroDois: number = 0;
+    let resultado: number = 0;
+
+    if(opcao == `+`){
+        numeroUm = parseInt(teclado(`Digite o 1º número `));
+        numeroDois = parseInt(teclado(`Digite o 2º número `));
+        resultado = numeroUm + numeroDois;
+        console.log(`O valor da soma de ${numeroUm} com ${numeroDois} `);
+        console.log(`é ${resultado}`);
+    }
+    
+    else if(opcao == `-`){
+        numeroUm = parseInt(teclado(`Digite o 1º número `));
+        numeroDois = parseInt(teclado(`Digite o 2º número `));
+        resultado = numeroUm - numeroDois;
+        console.log(`O valor da Subtração de ${numeroUm} com ${numeroDois} `),
+        console.log(`é ${resultado}`);
+    }
+    
+    else if(opcao == `*`){
+        numeroUm = parseInt(teclado(`Digite o 1º número: `));
+        numeroDois = parseInt(teclado(`Digite o 2º número: `));
+        resultado = numeroUm * numeroDois;
+        console.log(`O valor da multiplicação de ${numeroUm} com ${numeroDois} `);
+        console.log(`é ${resultado}`);
+    }
+    
+    else if(opcao == `/`){
+        numeroUm = parseInt(teclado(`Digite o 1º número: `));
+        numeroDois = parseInt(teclado(`Digite o 2º número `));
+        resultado = numeroUm / numeroDois;
+        console.log(`O valor da multiplicação de ${numeroUm} com ${numeroDois} `);
+        console.log(`é ${resultado}`);
+    }
+    
+    else{
+        console.log(`Algo deu errado!!`);
+    }
+    
+    console.log(`Digite para: `);
+    console.log(`0 - Sair`);
+    console.log(`1 - Continuar`);
+    inOut = parseInt(teclado(`Digite a opção `))
+
+} while (inOut != 0);
+
+console.log(`Fim de programa ...`);
+    
+
+
+
+
+        
